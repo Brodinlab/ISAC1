d3afbff2db6bd405969ffbf808b747b03b6158d3
Ziyang-Tan <yztanzy@gmail.com>
2024-10-21 17:33:48 +0200

    modify preparation.R

--- Scripts/Figure7/preparation.R
+++ Scripts/Figure7/preparation.R
@@ -5,61 +5,10 @@ library(SeuratDisk)
 library(rstatix)
 library(parallel)
 
-data_scTCR <- read_csv("data/scTCR_data_ISAC1.csv.gz") %>%
-    mutate(seurat_clusters = as.character(seurat_clusters))
-
-clone_id_map <- data_scTCR %>%
-    select(CDR3_concat, clone_id) %>%
-    unique()
-
-clone_exp <- data_scTCR %>%
-    group_by(CDR3_concat, Sample_Name) %>%
-    summarise(clone_count = n()) %>%
-    ungroup() %>%
-    inner_join(clone_id_map, by = "CDR3_concat")
-
-write_csv(clone_id_map, file = "results/clone_id_map.csv.gz")
-write_csv(clone_exp, file = "results/clone_expansion.csv.gz")
-
-
-obj <- LoadH5Seurat("/Users/tan/ISAC-TCR/figure reproduce/data/seurat_results_update2.h5Seurat")
-virus_specific_id <- read_csv(file = "Data/virus_specific_clone_id.csv") %>% pull()
-increase_clone <- c(48795, 37805, 2890, 45479, 53017, 33890)
-decrease_clone <- c(
-    7918, 30260, 51808, 32056, 4293, 41385, 18961, 50670, 41552,
-    47305, 20424, 18818, 47227, 32052, 43590, 32214, 55081
-)
-sub_name <- "CD8T"
-patient_name <- "ISAC02"
-obj_sub <- subset(obj, subset = patient_id == patient_name &
-    clone_id %in% union(increase_clone, decrease_clone) &
-    !clone_id %in% virus_specific_id &
-    cell_type == sub_name)
-tmp <- obj_sub[[]] %>%
-    mutate(
-        trend = case_when(
-            clone_id %in% virus_specific_id ~ "virus_specific",
-            clone_id %in% increase_clone ~ "increase",
-            clone_id %in% decrease_clone ~ "decrease",
-            TRUE ~ "other"
-        )
-    ) %>%
-    select(trend)
-rownames(tmp) <- obj_sub[[]]$unique_index
-obj_sub$trend <- tmp
-Idents(obj_sub) <- "trend"
-res <- FindMarkers(obj_sub,
-    ident.1 = "increase", ident.2 = "decrease",
-    logfc.threshold = 0
-)
-res$gene <- rownames(res)
-write_csv(res, "Data/Volcano compare trends in ISAC02.csv")
-
-
 # resampling DE analysis for Fig 7
 ## ranked list in Wu -----
 # cannot import to Seurat because the data is not raw counts
-df_wu <- read_csv("data/adult data from public/blood mCD8T from dataset_Wu.csv.gz")
+df_wu <- read_csv("single cell data/adult data from public/blood mCD8T from dataset_Wu.csv.gz")
 panel_wu <- colnames(df_wu)[1:3734]
 
 calculate_DE_tmp <- function(df, panel) {
@@ -107,10 +56,10 @@ rank_wu_mean <- lapply(rank_wu_list, function(x) {
     ) %>%
     ungroup() %>%
     arrange(desc(avg_rank_metric))
-write_csv(rank_wu_mean, "results/DE_resample_results/adults_Wu.csv")
+write_csv(rank_wu_mean, "Data/DE_resample_results/adults_Wu.csv")
 
 ## ranked list in Guo -----
-obj_guo_memt <- LoadH5Seurat("data/adult data from public/dataset_Guo_memt.h5Seurat")
+obj_guo_memt <- LoadH5Seurat("single cell data/adult data from public/dataset_Guo_memt.h5Seurat")
 
 # resampling (bootstrapping)
 rank_guo_list <- mclapply(paste("resample", 1:100), function(x) {
@@ -140,10 +89,10 @@ rank_guo_mean <- lapply(rank_guo_list, function(x) {
     ) %>%
     ungroup() %>%
     arrange(desc(avg_rank_metric))
-write_csv(rank_guo_mean, "results/DE_resample_results/adults_Guo.csv")
+write_csv(rank_guo_mean, "Data/DE_resample_results/adults_Guo.csv")
 
 ## ranked list in Zhang --------
-obj_zhang_memt <- LoadH5Seurat("data/adult data from public/dataset_Zhang_memt.h5Seurat")
+obj_zhang_memt <- LoadH5Seurat("single cell data/adult data from public/dataset_Zhang_memt.h5Seurat")
 
 # resampling (bootstrapping)
 rank_zhang_list <- mclapply(paste("resample", 1:100), function(x) {
@@ -173,11 +122,11 @@ rank_zhang_mean <- lapply(rank_zhang_list, function(x) {
     ) %>%
     ungroup() %>%
     arrange(desc(avg_rank_metric))
-write_csv(rank_zhang_mean, "results/DE_resample_results/adults_Zhang.csv")
+write_csv(rank_zhang_mean, "Data/DE_resample_results/adults_Zhang.csv")
 
 ## ranked list in ISAC -----------
 # exclude virus specific
-obj <- LoadH5Seurat("data/seurat_results_update2.h5Seurat") %>%
+obj <- LoadH5Seurat("single cell data/seurat_results_update2.h5Seurat") %>%
     subset(subset = cell_type == "CD8T" &
         virus_specific == "non_virus_specific" &
         TCR_Paired_Chains == 1)
@@ -213,7 +162,7 @@ for (i in unique(obj$tumor_type)) {
         ) %>%
         ungroup() %>%
         arrange(desc(avg_rank_metric))
-    write_csv(rank_sub_mean, paste0("results/DE_resample_results/ISAC_", i, ".csv"))
+    write_csv(rank_sub_mean, paste0("Data/DE_resample_results/ISAC_", i, ".csv"))
 }
 
 # calculate expansion ratio
@@ -250,4 +199,4 @@ df <- rbind(
         rename(patient = patient_id) %>%
         filter(expansion == "expanded")
 )
-write_csv(df, "results/adult_isac_expansion_ratio.csv")
+write_csv(df, "Data/adult_isac_expansion_ratio.csv")
